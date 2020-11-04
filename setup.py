eb1fa6d5e1bc558e6dc59f40169b8c772b95fd31
Jari Turkia <jmjt@jari.turkia.name>
2020-11-04 16:06:31 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='waste.d',
+    version='0.1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Jari Turkia',
+    author_email='',
+    description='',
+    install_requires=[
+        'Django'
+    ]
+)
