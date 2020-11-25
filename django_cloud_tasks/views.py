8d494e105c5573ee82ba81947b3de800e5384c22
Jari Turkia <jmjt@jari.turkia.name>
2020-11-25 17:13:17 +0200

    Improvement: Added checking for HTTP POST of Cloud Task data

--- django_cloud_tasks/views.py
+++ django_cloud_tasks/views.py
@@ -13,7 +13,12 @@ logger = logging.getLogger(__name__)
 
 @csrf_exempt
 def run_task(request):
-    body = json.loads(request.body.decode('utf-8'))
+    if request.method == 'POST':
+        if not request.body:
+            raise ValueError("Missing request body!")
+        body = json.loads(request.body.decode('utf-8'))
+    else:
+        raise ValueError("Nothing here!")
     request_headers = request.META
     task_name = request_headers.get('HTTP_X_APPENGINE_TASKNAME')
     task_queue_name = request_headers.get('HTTP_X_APPENGINE_QUEUENAME')
